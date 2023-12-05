6a8c8299bf805072d2f85d056ff0fb6de866180b
HOOOO98 <loading2us@gmail.com>
2023-12-05 12:28:01 +0900

    add color input & on off switch

--- src/App.js
+++ src/App.js
@@ -1,21 +1,33 @@
 import React, { useState } from "react";
+import "./App.css";
 
 function App() {
   const [color, setColor] = useState("rgb(0,0,0)");
+  const [onOff, setOnOff] = useState(false);
+
+  const isOnOff = {
+    text: onOff ? "켜짐" : "꺼짐",
+  };
 
   return (
     <div className="App">
-      <input
-        type="color"
-        value={color}
-        onChange={(e) => {
-          setColor(e.target.value);
-        }}
-      />
-
-      <div
-        style={{ width: "100px", height: "100px", backgroundColor: color }}
-      />
+      <section>
+        <input
+          id="color-picker"
+          type="color"
+          value={color}
+          onChange={(e) => {
+            setColor(e.target.value);
+          }}
+        />
+        <button style={{ color: color }} onClick={() => setColor("rgb(0,0,0)")}>
+          초기화
+        </button>
+      </section>
+      <section>
+        <p>{isOnOff.text}</p>
+        <button onClick={(e) => setOnOff(!onOff)}>on/off</button>
+      </section>
     </div>
   );
 }
